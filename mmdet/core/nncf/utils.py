a6a1cf00ae45ca23980d2b0cc08b91fc2102f9af
Aleksei Kashapov <aleksei.kashapov@intel.com>
2020-10-05 20:10:52 +0300

    Improve code style

--- mmdet/core/nncf/utils.py
+++ mmdet/core/nncf/utils.py
@@ -1,10 +1,7 @@
-import pathlib
 from collections import OrderedDict
 from contextlib import contextmanager
-from copy import deepcopy
 
 import torch
-from mmdet.utils import get_root_logger
 
 try:
     import nncf
@@ -25,128 +22,11 @@ def check_nncf_is_enabled():
 
 if is_nncf_enabled():
     try:
-        from nncf.initialization import InitializingDataLoader
-        from nncf.structures import QuantizationRangeInitArgs
-
-        from nncf import NNCFConfig
         from nncf import load_state
-        from nncf import create_compressed_model, register_default_init_args
-        from nncf.utils import get_all_modules
         from nncf.dynamic_graph.context import no_nncf_trace as original_no_nncf_trace
-        from nncf.nncf_network import NNCFNetwork
         from nncf.dynamic_graph.context import get_current_context
-
-        class_InitializingDataLoader = InitializingDataLoader
     except:
         raise RuntimeError("Incompatible version of NNCF")
-else:
-    class DummyInitializingDataLoader:
-        pass
-
-    class_InitializingDataLoader = DummyInitializingDataLoader
-
-def unwrap_module_from_nncf_if_required(module):
-    if not is_nncf_enabled():
-        return module
-    if isinstance(module, NNCFNetwork):
-        return module.get_nncf_wrapped_model()
-    return module
-
-class MMInitializeDataLoader(class_InitializingDataLoader):
-    def get_inputs(self, dataloader_output):
-        # redefined InitializingDataLoader because
-        # of DataContainer format in mmdet
-        kwargs = {k: v.data[0] for k, v in dataloader_output.items()}
-        return (), kwargs
-
-    # TODO: not tested; need to test
-    def get_target(self, dataloader_output):
-        return dataloader_output["gt_bboxes"], dataloader_output["gt_labels"]
-
-def wrap_nncf_model(model, cfg, data_loader_for_init=None, get_fake_input_func=None,
-                    should_use_dummy_forward_with_export_part=True):
-    """
-    The function wraps mmdet model by NNCF
-    Note that the parameter `get_fake_input_func` should be the function `get_fake_input`
-    -- cannot import this function here explicitly
-    """
-    check_nncf_is_enabled()
-    pathlib.Path(cfg.work_dir).mkdir(parents=True, exist_ok=True)
-    nncf_config = NNCFConfig(cfg.nncf_config)
-    logger = get_root_logger(cfg.log_level)
-
-    if data_loader_for_init:
-        wrapped_loader = MMInitializeDataLoader(data_loader_for_init)
-        # TODO: [NNCF] need check the arguments in register_default_init_args()
-        # TODO: add loss factory that reads config file, creates them and passes to register_default_init_args()
-        nncf_config.register_extra_structs([QuantizationRangeInitArgs(wrapped_loader)])
-    elif not cfg.nncf_load_from:
-        raise RuntimeError("Tried to load NNCF checkpoint, but there is no path")
-
-    if cfg.nncf_load_from:
-        resuming_state_dict = load_checkpoint(model, cfg.nncf_load_from)
-        logger.info(f"loaded nncf checkpoint from {cfg.nncf_load_from}")
-    else:
-        resuming_state_dict = None
-
-    def __get_fake_data_for_forward(cfg, nncf_config, get_fake_input_func):
-        # based on the method `export` of BaseDetector from mmdet/models/detectors/base.py
-        # and on the script tools/export.py
-        assert get_fake_input_func is not None
-
-        input_size = nncf_config.get("input_info").get('sample_size')
-        assert len(input_size) == 4 and input_size[0] == 1
-
-        H, W = input_size[-2:]
-        C = input_size[1]
-        orig_img_shape = tuple([H, W, C]) #HWC order here for np.zeros to emulate cv2.imread
-
-        device = next(model.parameters()).device
-
-        # NB: the full cfg is required here!
-        fake_data = get_fake_input_func(cfg, orig_img_shape=orig_img_shape, device=device)
-        return fake_data
-    def _get_fake_data_for_forward():
-        # make a closure to use config and get_fake_input_func from the external scope
-        cfg_copy = deepcopy(cfg)
-        nncf_config = NNCFConfig(cfg_copy.nncf_config)
-        return __get_fake_data_for_forward(cfg_copy, nncf_config, get_fake_input_func)
-
-    def dummy_forward_without_export_part(model):
-        # based on the method `export` of BaseDetector from mmdet/models/detectors/base.py
-        # and on the script tools/export.py
-        fake_data = _get_fake_data_for_forward()
-        img = fake_data["img"]
-        img_metas = fake_data["img_metas"]
-        with model.forward_dummy_context(img_metas):
-            model(img)
-
-    def dummy_forward_with_export_part(model):
-        # based on the method `export` of BaseDetector from mmdet/models/detectors/base.py
-        # and on the script tools/export.py
-        fake_data = _get_fake_data_for_forward()
-        img = fake_data["img"]
-        img_metas = fake_data["img_metas"]
-        with model.forward_export_context(img_metas):
-            model(img)
-
-    if "nncf_should_use_dummy_forward_with_export_part" in cfg:
-        # TODO: this parameter is for debugging, remove it later
-        should_use_dummy_forward_with_export_part = cfg.get("nncf_should_use_dummy_forward_with_export_part")
-        logger.info(f"set should_use_dummy_forward_with_export_part={should_use_dummy_forward_with_export_part}")
-
-    if should_use_dummy_forward_with_export_part:
-        logger.info(f"dummy_forward = dummy_forward_with_export_part")
-        dummy_forward = dummy_forward_with_export_part
-    else:
-        logger.info(f"dummy_forward = dummy_forward_without_export_part")
-        dummy_forward = dummy_forward_without_export_part
-
-    model.dummy_forward_fn = dummy_forward
-
-    compression_ctrl, model = create_compressed_model(model, nncf_config, dummy_forward_fn=dummy_forward,
-                                                      resuming_state_dict=resuming_state_dict)
-    return compression_ctrl, model
 
 
 def load_checkpoint(model, filename, map_location=None, strict=False):
@@ -174,13 +54,6 @@ def load_checkpoint(model, filename, map_location=None, strict=False):
     return checkpoint
 
 
-def export_model_to_onnx(compression_ctrl, f_name):
-    logger = get_root_logger(cfg.log_level)
-    logger.error("The function 'mmdet.core.nncf.export_model_to_onnx' is obsolete now "
-                 "-- please, use the script tools/export.py with the same config file and the corresponding snapshot")
-    logger.error("Now the function 'mmdet.core.nncf.export_model_to_onnx' does nothing and return")
-
-
 @contextmanager
 def nullcontext():
     """
@@ -193,9 +66,11 @@ def no_nncf_trace():
     """
     Wrapper for original NNCF no_nncf_trace() context
     """
+
     if is_nncf_enabled():
         return original_no_nncf_trace()
     return nullcontext()
+
 
 def is_in_nncf_tracing():
     if not is_nncf_enabled():
