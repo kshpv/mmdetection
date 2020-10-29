3a439ec40ce55f25ff3953fd76ffc2304f01b320
Aleksei Kashapov <aleksei.kashapov@intel.com>
2020-10-29 23:44:41 +0300

    Add description and improve code style

--- mmdet/integration/nncf/compression.py
+++ mmdet/integration/nncf/compression.py
@@ -111,6 +111,21 @@ def wrap_nncf_model(model,
     else:
         resuming_state_dict = None
 
+    if "nncf_compress_postprocessing" in cfg:
+        # NB: This parameter is used to choose if we should try to make NNCF compression
+        #     for a whole model graph including postprocessing (`dummy_forward_with_export_part`),
+        #     or make NNCF compression of the part of the model without postprocessing
+        #     (`dummy_forward_without_export_part`).
+        #     Our primary goal is to make NNCF compression of such big part of the model as
+        #     possible, so `dummy_forward_with_export_part` is our primary choice, whereas
+        #     `dummy_forward_without_export_part` is our fallback decision.
+        #     When we manage to enable NNCF compression for sufficiently many models,
+        #     we should keep one choice only.
+        nncf_compress_postprocessing = cfg.get('nncf_compress_postprocessing')
+        logger.debug('set should_compress_postprocessing='f'{nncf_compress_postprocessing}')
+    else:
+        nncf_compress_postprocessing = True
+
     def _get_fake_data_for_forward(cfg, nncf_config, get_fake_input_func):
         input_size = nncf_config.get("input_info").get('sample_size')
         assert get_fake_input_func is not None
@@ -131,12 +146,6 @@ def wrap_nncf_model(model,
             logger.debug(f"NNCF will NOT compress a postprocessing part of the model")
         with ctx:
             model(img)
-
-    if "nncf_compress_postprocessing" in cfg:
-        nncf_compress_postprocessing = cfg.get('nncf_compress_postprocessing')
-        logger.debug('set should_compress_postprocessing='f'{nncf_compress_postprocessing}')
-    else:
-        nncf_compress_postprocessing = False
 
     model.dummy_forward_fn = dummy_forward
 
